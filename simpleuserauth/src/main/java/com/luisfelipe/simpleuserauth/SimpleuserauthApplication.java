6e999fdf15ffdf84933d91177f47ef3ab2a94aec
LuisFelipe7215 <170734954+LuisFelipe7215@users.noreply.github.com>
2026-05-22 14:55:45 -0300

    init spring boot app

---
+++ simpleuserauth/src/main/java/com/luisfelipe/simpleuserauth/SimpleuserauthApplication.java
@@ -0,0 +1,13 @@
+package com.luisfelipe.simpleuserauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleuserauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleuserauthApplication.class, args);
+	}
+
+}
